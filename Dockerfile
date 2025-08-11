22e00ef34a41c96bdb8c70a09aaf4c2673a9468b
Taali <matrebacz125@gmail.com>
2025-08-11 22:51:35 +0200

    auth addittions
    Added tokenization and authorization decorator

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,9 @@ COPY . .
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
+ENV SECRET_KEY=${SECRET_KEY}
+ENV ALGORITHM=${ALGORITHM}
+ENV DATABASE_URL=${DATABASE_URL}
+ENV TOKEN_EXPIRE_IN_MINUTES=${TOKEN_EXPIRE_IN_MINUTES}
+
 CMD ["uvicorn", "main:app", "--host", "127.0.0.1", "--port", "8000"]
--- views/auth.py
+++ views/auth.py
@@ -1,3 +1,52 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status, Request
+from fastapi.responses import RedirectResponse
+
+from functools import wraps
+import os
+from datetime import datetime, timedelta
+
+from hashlib import sha1
+import jwt
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+TOKEN_EXPIRE_IN_MINUTES = os.getenv("TOKEN_EXPIRE_IN_MINUTES")
 
 router = APIRouter(prefix="/auth", tags=["auth"])
+
+def decode_token(token: str) -> str:
+    try:
+        if not token:
+            raise HTTPException(status.HTTP_400_BAD_REQUEST, "Token is required.")
+
+        payload = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Expired token.")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Invalid token.")
+
+def create_token(data: dict, expire: timedelta = timedelta(minutes=TOKEN_EXPIRE_IN_MINUTES)):
+    to_encode = data.copy()
+    expire_time = datetime.utcnow() + expire
+    to_encode.update({"exp": expire_time})
+    token = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
+    return token
+
+# TODO: Add autorization for 2 levels. author, participant 
+
+def auth_required(func):
+    @wraps(func)
+    def wrapper(*args, request: Request, **kwargs):
+        token = request.session.get("token")
+
+        if not token:
+            return RedirectResponse("/login", status_code=303)
+        
+        try:
+            decode_token(token)
+        except HTTPException:
+            return RedirectResponse("/login", status_code=303)
+
+        return func(*args, request=request, **kwargs)
+    return wrapper
