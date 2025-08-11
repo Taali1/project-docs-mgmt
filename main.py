f4039bbed41f4062165254386504dfe143e9ea63
Taali <matrebacz125@gmail.com>
2025-08-11 19:04:40 +0200

    added blueprints

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
+from views.auth import router as router_auth
+from views.project import router as router_project
+from views.document import router as router_document
+
+
 app = FastAPI()
 
 if __name__ == "__main__":
-    pass
+    app.include_router(router_auth)
+    app.include_router(router_project)
+    app.include_router(router_document)
