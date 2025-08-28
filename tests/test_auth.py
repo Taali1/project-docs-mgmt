a5865fefdaa99231f80ea0fb44ec26b80d26b750
Taali <matrebacz125@gmail.com>
2025-08-28 22:21:59 +0200

    Created tests for auth route (register user)

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,21 +1,44 @@
 import pytest
-from fastapi.testclient import TestClient
-from main import app, SECRET_KEY, ALGORITHM
-import jwt
+from tests.test_data import users_test_data
 
 
-client = TestClient(app)
+@pytest.mark.parametrize("user_id, password", users_test_data)
+def test_auth_success(client, mocker, user_id, password):
+    mocker.patch("main.select_user", return_value=None)
+    mocker.patch("main.insert_user", return_value=None)
 
-def test_auth_success():
-    response = client.post("/auth", params={"username": "login", "password": "password"})
+    payload = {
+        "user_id": user_id,
+        "password": password,
+        "repeat_password": password
+    }
+
+    response = client.post("/auth", json=payload)
+    
     assert response.status_code == 201
+    assert response.text == "Registerd succesfuly"
 
-def test_auth_fail():
-    response = client.post("/auth", params={"username": "login", "password": ""})
-    assert response.status_code == 400
+@pytest.mark.parametrize("user_id, password", users_test_data)
+def test_auth_fail(client, mocker, user_id, password):
+    mocker.patch("main.select_user", return_value=None)
+    mocker.patch("main.insert_user", return_value=None)
 
-    response = client.post("/auth", params={"username": "", "password": "password"})
+    response = client.post("/auth", json={"user_id": user_id, "password": "", "repeat_password": password})
     assert response.status_code == 400
+    assert response.json()["detail"] == "Password is required"
 
-    response = client.post("/auth", params={"username": "login", "password": ""})
+    response = client.post("/auth", json={"user_id": "", "password": password, "repeat_password": password})
     assert response.status_code == 400
+    assert response.json()["detail"] == "Login is required"
+
+    response = client.post("/auth", json={"user_id": user_id, "password": password, "repeat_password": ""})
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Repeat password is required"
+
+    response = client.post("/auth", json={"user_id": user_id, "password": password + "_changed", "repeat_password": password})
+    assert response.status_code == 400
+    assert response.json()["detail"] == "Password and Repeat password are not the same"
+
+
+
+
